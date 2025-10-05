0430d3bcd06951725e7f7630fcb0e7bfbb7fffbf
Chun Sokna <chunsokna111@gmail.com>
2025-10-05 14:40:05 +0700

    Refactor API routing in bootstrap/app.php to use a closure for grouping routes, and update routes/api.php to include versioned user routes from api/v1.php.

---
+++ routes/api/v1.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'v1', 'as' => 'v1.'], function () {
+    Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+        // Public routes
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+
+        // Protected routes
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::delete('logout', [AuthController::class, 'logout']);
+            Route::get('user', [AuthController::class, 'me']);
+        });
+    });
+});
